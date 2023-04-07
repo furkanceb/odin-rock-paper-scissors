976deb637683860b3de62356dfbecf8c8b498a23
furkanceb <117816558+furkanceb@users.noreply.github.com>
2023-04-07 20:59:59 +0300

    Add function game()

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
 
-function  getComputerChoice(){
-    const randomNumber= Math.floor(Math.random()*3);
-    let computerSelection;
-    
-    switch(randomNumber){
-    
-        case 0:
-        computerSelection="Rock";
-        break;
-        
-        case 1:
-        computerSelection="Paper";
-        break;
-        
-        case 2:
-        computerSelection="Scissors"
-        break;    
-    }
-    return computerSelection;
-}
 
-function getPlayerChoice(){
-let playerSelection=prompt("Enter your selection(Rock/Paper/Scissors):)");
-
-playerSelection=playerSelection.toLowerCase();
-playerSelection=playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase());
-return playerSelection;
-}
-
-const computerSelection=getComputerChoice();
-const playerSelection=getPlayerChoice();
 
 function playRound(playerSelection,computerSelection){
     
     
     if(playerSelection===computerSelection){
-        console.log("Draw")
+        console.log("Draw!")
     }
     else  if (playerSelection==="Rock" && computerSelection==="Scissors" || playerSelection==="Paper" && computerSelection==="Rock" || playerSelection==="Scissors" && computerSelection==="Paper"){
-        console.log("Player won")
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
     }
 
     else{
-        console.log("Computer won")
+        console.log(`You lost! ${computerSelection} beats ${playerSelection}`);
     }
 }
 
-console.log(computerSelection)
-playRound(playerSelection,computerSelection)
+
+function game(){
+    for(let i=0;i<5;i++){
+        function  getComputerChoice(){
+            const randomNumber= Math.floor(Math.random()*3);
+            let computerSelection;
+            
+            switch(randomNumber){
+            
+                case 0:
+                computerSelection="Rock";
+                break;
+                
+                case 1:
+                computerSelection="Paper";
+                break;
+                
+                case 2:
+                computerSelection="Scissors"
+                break;    
+            }
+            return computerSelection;
+        }
+        
+        function getPlayerChoice(){
+        let playerSelection=prompt("Enter your selection(Rock/Paper/Scissors):)");
+        
+        playerSelection=playerSelection.toLowerCase();
+        playerSelection=playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase());
+        return playerSelection;
+        }
+        
+        const computerSelection=getComputerChoice();
+        const playerSelection=getPlayerChoice();
+    
+        playRound(playerSelection,computerSelection);
+}
+}
+game()
