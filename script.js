c3e926038043a71f3dd0b7acf4c1499388b60bd3
furkanceb <117816558+furkanceb@users.noreply.github.com>
2023-04-07 16:51:33 +0300

    Make playerSelection case-insensitive

--- script.js
+++ script.js
@@ -20,14 +20,18 @@ function  getComputerChoice(){
     return computerSelection;
 }
 
-const playerSelection=prompt("Enter your selection(Rock/Paper/Scissors):)");
+let playerSelection=prompt("Enter your selection(Rock/Paper/Scissors):)");
+playerSelection=playerSelection.toLowerCase();
+playerSelection=playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase());//not sure
 const computerSelection=getComputerChoice();
 
 
 function playRound(playerSelection,computerSelection){
+    
+    
     if(playerSelection===computerSelection){
         console.log("Draw")
-  }
+    }
     else  if (playerSelection==="Rock" && computerSelection==="Scissors" || playerSelection==="Paper" && computerSelection==="Rock" || playerSelection==="Scissors" && computerSelection==="Paper"){
         console.log("Player won")
     }
